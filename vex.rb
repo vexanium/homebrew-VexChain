ddb3aea8a2722a2c5f84a9d1c2df2bca04ec4c54
Yuda Adi Pratama <61916091+YudaAdiPratama@users.noreply.github.com>
2022-11-04 20:37:51 +0700

    Update vex.rb

--- vex.rb
+++ vex.rb
@@ -11,12 +11,12 @@ class Vex < Formula
    depends_on "openssl@1.1"
    depends_on "libusb"
    depends_on "libpqxx"
-   depends_on :macos => :mojave
+   depends_on :macos => :catalina
    depends_on :arch =>  :intel
 
    bottle do
       root_url "https://github.com/vexanium/VexChain/releases/download/v2.1.0"
-      sha256 "5f1eb3639ab4d998dbd3023e7a9c1c70b7faf9914608cdcf7712342c864e2b31" => :Ventura
+      sha256 "5f1eb3639ab4d998dbd3023e7a9c1c70b7faf9914608cdcf7712342c864e2b31" => :ventura
    end
    def install
       raise "Error, only supporting binary packages at this time"
