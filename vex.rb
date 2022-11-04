5b89332aab07e11805a2140031fc58a20af3138f
Yuda Adi Pratama <61916091+YudaAdiPratama@users.noreply.github.com>
2022-11-04 20:21:05 +0700

    Create vex.rb

---
+++ vex.rb
@@ -0,0 +1,25 @@
+class Vex < Formula
+
+   homepage "https://github.com/vexanium/vexchain2"
+   revision 0
+   url "https://github.com/vexanium/VexChain/archive/v2.1.0.tar.gz"
+   version "2.1.0"
+
+   option :universal
+
+   depends_on "gmp"
+   depends_on "openssl@1.1"
+   depends_on "libusb"
+   depends_on "libpqxx"
+   depends_on :macos => :mojave
+   depends_on :arch =>  :intel
+
+   bottle do
+      root_url "https://github.com/vexanium/VexChain/releases/download/v2.1.0"
+      sha256 "5f1eb3639ab4d998dbd3023e7a9c1c70b7faf9914608cdcf7712342c864e2b31" => :Ventura
+   end
+   def install
+      raise "Error, only supporting binary packages at this time"
+   end
+end
+__END__
